d36cd46a9121db517cffb1449be8fd07051e2699
Adam <adam.l.shannon@gmail.com>
2018-07-23 13:46:25 +0100

    Matplotlib test added and python3-tk now installed as well

--- CreateMLEnv.sh
+++ CreateMLEnv.sh
@@ -7,5 +7,5 @@ sudo apt-get -y update
 sudo apt-get -y install python3.7
 sudo apt-get -y install python3-pip
 
-sudo apt-get install -y build-essentials python-numpy python-dev cmake zlib1g-dev libjpeg-dev xvfb xorg-dev python-opengl libboost-all-dev libsdl2-dev swig
+sudo apt-get install -y build-essentials python-numpy python-dev python3-tk cmake zlib1g-dev libjpeg-dev xvfb xorg-dev python-opengl libboost-all-dev libsdl2-dev swig
 yes | pip3 install numpy scipy sklearn cython pandas matplotlib seaborn tensorflow keras ipython jupyter 'gym[all]'
